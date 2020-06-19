cc37504478825350c8ee4a7b7c3010719e8324c3
Enes Bilal Seker <32172926+enesbilalseker@users.noreply.github.com>
2020-06-19 17:19:59 +0300

    fresh

--- JumpTheBallGame/Assets/Scripts/CubukKontrol.cs
+++ JumpTheBallGame/Assets/Scripts/CubukKontrol.cs
@@ -15,7 +15,16 @@ public class CubukKontrol : MonoBehaviour
         transform.Translate(new Vector3(0, -fallSpeed * Time.deltaTime, 0));
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal")* speed * Time.deltaTime, 0, 0));
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal")* speed * 0.0182451f, 0, 0));
+        }
+
+        if (transform.position.x >= 15.688573f)
+        {
+            transform.position = new Vector3(-14.324997f, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x <= -15.688573f)
+        {
+            transform.position = new Vector3(14.324997f, transform.position.y, transform.position.z);
         }
     }
 }
